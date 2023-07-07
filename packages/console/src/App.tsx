5e2b0900ee82167fb0b57f90cc60fd7e4a9907b0
shashimalcse <shashimalsenarath.17@cse.mrt.ac.lk>
2023-07-07 14:03:10 +0530

    add opa policy generator

--- packages/console/src/App.tsx
+++ packages/console/src/App.tsx
@@ -1,22 +1,69 @@
-import ReactFlow , { Background, MiniMap,
-  Controls, BackgroundVariant } from 'reactflow';
+import ReactFlow, {
+  Background, MiniMap,
+  Controls, BackgroundVariant
+} from 'reactflow';
+import { PathType, getRegoPolicy } from "@policytunnel/core/src/policy_handler/convertor";
 import './App.css'
 import 'reactflow/dist/style.css';
+import { useState } from 'react';
 
 function App() {
 
-const initialNodes = [
-  { id: '1', position: { x: 0, y: 0 }, data: { label: '1' } },
-  { id: '2', position: { x: 0, y: 100 }, data: { label: '2' } },
-];
-const initialEdges = [{ id: 'e1-2', source: '1', target: '2' }];
+  const initialNodes = [
+    { id: '1', position: { x: 0, y: 0 }, data: { label: '1' } },
+    { id: '2', position: { x: 0, y: 100 }, data: { label: '2' } },
+  ];
+  const initialEdges = [{ id: 'e1-2', source: '1', target: '2' }];
+  const jsonString = `{
+    "validators": [
+      {
+        "name": "conditional",
+        "conf": {
+          "if": [
+            {
+              "conf": {
+                "fields": [
+                  {
+                    "comparator": "equals",
+                    "field": "authnCtx.gender",
+                    "value": "female"
+                  }
+                ]
+              },
+              "name": "attributes"
+            }
+          ],
+          "then": {
+            "conf": {},
+            "name": "false"
+          },
+          "else": {
+            "conf": {},
+            "name": "true"
+          }
+        }
+      }
+    ]
+  }`
+  const [result, setResult] = useState(''); 
+  const handleButtonClick = () => {
+    // Function logic goes here
+    const output = getRegoPolicy(jsonString, PathType.Allow)
+    setResult(output);
+  };
   return (
-    <div style={{ width: '100vw', height: '100vh'}}>
-      <ReactFlow nodes={initialNodes} edges={initialEdges}>
-        <Background color="#FFFFFF" variant={BackgroundVariant.Dots} />
-        <Controls />
-        <MiniMap />
-      </ReactFlow>
+    <div style={{ display: 'flex', height: '100vh' }}>
+      <div style={{ width: '80vw'}}>
+        <ReactFlow nodes={initialNodes} edges={initialEdges}>
+          <Background color="#FFFFFF" variant={BackgroundVariant.Dots} />
+          <Controls />
+          <MiniMap />
+        </ReactFlow>
+      </div>
+      <div style={{ width: '20vw', backgroundColor: 'gray' }}>
+        <button onClick={handleButtonClick}>Genarate OPA policy</button>
+        <p>{result}</p>
+      </div>
     </div>
   )
 }
--- packages/core/src/policy_handler/convertor.ts
+++ packages/core/src/policy_handler/convertor.ts
@@ -1,3 +1,5 @@
+import { readFileSync, writeFileSync } from 'fs';
+
 export interface Input {
    validators: Validator[] | null
 }
@@ -33,29 +35,52 @@ export const getRegoPolicy = (jsonString: string, pathType:PathType): any => {
    const input: Input = JSON.parse(jsonString)
    const paths = findPaths(input, pathType);
    const conditions: any[] = [];
+   const policies = [
+      "package example",
+      "import future.keywords.if",
+      "default allow := false"
+   ]
    paths.forEach(function (value) {
       const pathConditions: any = traversePath(input, value, pathType);
       console.log("positive")
       pathConditions.positive.forEach( function(value: ConditionalEntity[]) {
+         const opaPositiveConditions: string[] = [
+            "allow {"
+         ]
          value.forEach(function (ifCon) {
             const opaCondition = getOpaCondition(ifCon)
+            policies.push(opaCondition)
+            opaPositiveConditions.push(ifCon.name + "_" + ifCon.conf.fields![0].comparator)
          })
+         opaPositiveConditions.push("}")
+         const policyPositiveContent = opaPositiveConditions.join('\n');
+         policies.push(policyPositiveContent)
 
       })
       console.log("negative")
       pathConditions.negative.forEach( function(value: ConditionalEntity[]) {
+         const opaNegativeConditions: string[] = [
+            "allow {"
+         ]
          value.forEach(function (ifCon) {
             const opaCondition = getOpaCondition(ifCon)
+            policies.push(opaCondition)
+            opaNegativeConditions.push("not " + ifCon.name + "_" + ifCon.conf.fields![0].comparator)
          })
+         opaNegativeConditions.push("}")
+         const policyNegativeContent = opaNegativeConditions.join('\n');
+         policies.push(policyNegativeContent)
       })
       conditions.push(pathConditions)
    })
-   return conditions
+   const policiesContent = policies.join('\n');
+   console.log(policiesContent)
+   return policiesContent
 }
 
 function getOpaCondition(condition: ConditionalEntity): string {
    
-
+   let opaCondition: string = "";
    if (condition.conf.fields) {
       switch (condition.conf.fields[0].comparator) {
          case "contains": {
@@ -63,12 +88,12 @@ function getOpaCondition(condition: ConditionalEntity): string {
             break;
          }
          case "equals": {
-            console.log("equals")
+            opaCondition = condition.name + "_" + condition.conf.fields[0].comparator + " if input." + condition.conf.fields[0].field + " == \"" + condition.conf.fields[0].value + "\"";
             break;
          }
       }
    }
-   return ""
+   return opaCondition
 }
 
 function findPaths(input: Input, pathType: PathType): string[] {
