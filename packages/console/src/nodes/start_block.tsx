de1cb943844f0d84c9af950ab90aaf87a1bd0e9c
Thilina Shashimal Senarath <thilinasenarath97@gmail.com>
2023-08-05 14:21:13 +0530

    add a custom handle

--- packages/console/src/nodes/start_block.tsx
+++ packages/console/src/nodes/start_block.tsx
@@ -1,4 +1,5 @@
 import { Handle, Position } from 'reactflow';
+import AddIcon from '@mui/icons-material/Add';
 
 
 function StartBlock() {
@@ -7,12 +8,19 @@ function StartBlock() {
       <div className='flex justify-center items-center w-16 h-16 rounded-full border-4 border-white shadow-md bg-black font-sans text-white text-xs'>
         Start
       </div>
-      <Handle
-        type="source"
-        position={Position.Right}
-        id="a"
-        isConnectable={true}
-      />
+      <div className='absolute flex justify-center items-center bg-black rounded-full' style={{ top: '50%', right: '-15px', transform: 'translateY(-50%)' }}>
+        <Handle
+          type="source"
+          position={Position.Right}
+          id="a"
+          isConnectable={true}
+          className='z-40 p-[8px] bg-transparent border-transparent'
+        />
+        <div className='flex items-center justify-center w-4 h-4'>
+          <AddIcon style={{fontSize: '10px', color: 'white'}}/>    
+        </div>
+      </div>
+
     </>
   );
 }
