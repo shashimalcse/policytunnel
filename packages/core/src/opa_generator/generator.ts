c649cda8bc963501cd4fb0b4ae0c78f51fb02653
Thilina Shashimal Senarath <thilinasenarath97@gmail.com>
2023-09-03 01:08:30 +0530

    add OPA contains and at least ones

--- packages/core/src/opa_generator/generator.ts
+++ packages/core/src/opa_generator/generator.ts
@@ -7,7 +7,7 @@ export function getOpaPolicy(graph: Graph, paths: PathNode[][]): string {
 
     const policies = [
         "package example",
-        "import future.keywords.if",
+        "import future.keywords",
         "",
         "default allow := false",
         ""
@@ -30,9 +30,9 @@ export function getOpaPolicy(graph: Graph, paths: PathNode[][]): string {
                     const opaCondition = getOpaCondition(properties)
                     policies.push(opaCondition)
                     if (isNegativeCondition(path[i+1])) {
-                        opaConditions.push("    not " + properties.attribute.name + "_" + properties.operator)
+                        opaConditions.push("    not " + properties.attribute.name.replace(/\./g, "_") + "_" + properties.operator)
                     } else {
-                        opaConditions.push("    " + properties.attribute.name + "_" + properties.operator)
+                        opaConditions.push("    " + properties.attribute.name.replace(/\./g, "_") + "_" + properties.operator)
                     }
                 }
             }
@@ -63,8 +63,27 @@ function getOpaCondition(properties: IfNodeProperties): string {
             opaCondition = properties.attribute.name + "_" + properties.operator + " if input." + properties.attribute.name + " == \"" + properties.value + "\"";
             break;
         }
-        case "notEqual": {
+        case "not_equal": {
             opaCondition = properties.attribute.name + "_" + properties.operator + " if input." + properties.attribute.name + " != \"" + properties.value + "\"";
+            break;
+        }
+        case "contains": {
+            var values: string[] = Array.isArray(properties.value) ? properties.value : [];
+            const valueList = `${properties.attribute.name.replace(/\./g, "_") + "_" + properties.operator + "_list"} := {${values.map(value => `"${value}"`).join(", ")}}`;
+            opaCondition = `${valueList}\n${properties.attribute.name.replace(/\./g, "_") + "_" + properties.operator} if {\n` +
+            `   every value in ${properties.attribute.name.replace(/\./g, "_") + "_" + properties.operator + "_list"} {\n` +
+            `    value in input.${properties.attribute.name}\n` +
+            `   }\n` +
+            `}`  ;
+            break;
+        }
+        case "contain_at_least_one": {
+            var values: string[] = Array.isArray(properties.value) ? properties.value : [];
+            const valueList = `${properties.attribute.name.replace(/\./g, "_") + "_" + properties.operator + "_list"} := {${values.map(value => `"${value}"`).join(", ")}}`;
+            opaCondition = `${valueList}\n${properties.attribute.name.replace(/\./g, "_") + "_" + properties.operator} if {\n` +
+            `   some value in input.${properties.attribute.name} \n` +
+            `       value in ${properties.attribute.name.replace(/\./g, "_") + "_" + properties.operator + "_list"}\n`+
+            `}`;
             break;
         }
     }
