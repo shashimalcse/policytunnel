2942dd469be16bae49c2f3ce92cb55de3c445c45
Thilina Shashimal Senarath <thilinasenarath97@gmail.com>
2023-09-03 14:29:25 +0530

    add opa for not contains

--- packages/core/src/opa_generator/generator.ts
+++ packages/core/src/opa_generator/generator.ts
@@ -30,9 +30,17 @@ export function getOpaPolicy(graph: Graph, paths: PathNode[][]): string {
                     const opaCondition = getOpaCondition(properties)
                     policies.push(opaCondition)
                     if (isNegativeCondition(path[i+1])) {
-                        opaConditions.push("    not " + properties.attribute.name.replace(/\./g, "_") + "_" + properties.operator)
+                        if (properties.operator == "not_contains" || properties.operator == "not_contain_at_least_one") {
+                            opaConditions.push("    " + properties.attribute.name.replace(/\./g, "_") + "_" + properties.operator)
+                        } else {
+                            opaConditions.push("    not " + properties.attribute.name.replace(/\./g, "_") + "_" + properties.operator)
+                        }
                     } else {
-                        opaConditions.push("    " + properties.attribute.name.replace(/\./g, "_") + "_" + properties.operator)
+                        if (properties.operator == "not_contains" || properties.operator == "not_contain_at_least_one") {
+                            opaConditions.push("    not " + properties.attribute.name.replace(/\./g, "_") + "_" + properties.operator)
+                        } else {
+                            opaConditions.push("    " + properties.attribute.name.replace(/\./g, "_") + "_" + properties.operator)
+                        }
                     }
                 }
             }
@@ -67,7 +75,9 @@ function getOpaCondition(properties: IfNodeProperties): string {
             opaCondition = properties.attribute.name.replace(/\./g, "_") + "_" + properties.operator + " if input." + properties.attribute.name + " != \"" + properties.value + "\"";
             break;
         }
-        case "contains": {
+        case "contains":
+        case "not_contains":
+        {
             var values: string[] = Array.isArray(properties.value) ? properties.value : [];
             const valueList = `${properties.attribute.name.replace(/\./g, "_") + "_" + properties.operator + "_list"} := {${values.map(value => `"${value}"`).join(", ")}}`;
             opaCondition = `${valueList}\n${properties.attribute.name.replace(/\./g, "_") + "_" + properties.operator} if {\n` +
@@ -77,7 +87,8 @@ function getOpaCondition(properties: IfNodeProperties): string {
             `}`  ;
             break;
         }
-        case "contain_at_least_one": {
+        case "contain_at_least_one": 
+        case "not_contain_at_least_one": {
             var values: string[] = Array.isArray(properties.value) ? properties.value : [];
             const valueList = `${properties.attribute.name.replace(/\./g, "_") + "_" + properties.operator + "_list"} := {${values.map(value => `"${value}"`).join(", ")}}`;
             opaCondition = `${valueList}\n${properties.attribute.name.replace(/\./g, "_") + "_" + properties.operator} if {\n` +
