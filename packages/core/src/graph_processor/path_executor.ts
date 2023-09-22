85121453dfbdb240400e33e8bb40e50608977948
Thilina Shashimal Senarath <thilinasenarath97@gmail.com>
2023-09-22 07:27:27 +0530

    fix else path issue in path executor

--- packages/core/src/graph_processor/path_executor.ts
+++ packages/core/src/graph_processor/path_executor.ts
@@ -9,7 +9,8 @@ export function executePaths(input: string, graph: Graph, paths: PathNode[][]): 
     for (const path of paths) {
         let allConditionsMet = true;
         // We assume all conditions are met until proven otherwise
-        for (const node of path) {
+        for (let i = 0; i < path.length; i++) {
+            const node = path[i];
             if (node.type === BlockType.IF) {
                 const nodeToExecute = graph.nodes.find(n => n.id === node.nodeId);
                 if (!nodeToExecute?.properties) {
@@ -17,54 +18,107 @@ export function executePaths(input: string, graph: Graph, paths: PathNode[][]): 
                 } else {
                     const properties: IfNodeProperties = nodeToExecute?.properties as IfNodeProperties;
                     const inputValue = getValueFromInput(json, properties.attribute.name);
-                    switch (properties.operator) {
-                        case "equal":
-                            if (inputValue !== properties.value) {
+                    const isNeg = isNegativeCondition(path[i+1])
+                    if (isNeg) {
+                        switch (properties.operator) {
+                            case "equal":
+                                if (inputValue === properties.value) {
+                                    allConditionsMet = false;
+                                    break;
+                                }
+                                break;
+                            case "not_equal":
+                                if (inputValue !== properties.value) {
+                                    allConditionsMet = false;
+                                    break;
+                                }
+                                break;
+                            case "contains":
+                                var values: string[] = Array.isArray(properties.value) ? properties.value : [];
+                                var inputValues: string[] = Array.isArray(inputValue) ? inputValue : [];
+                                if (values.every(elementA => inputValues.includes(elementA))) {
+                                    allConditionsMet = false;
+                                    break;
+                                }
+                                break;
+                            case "not_contains":
+                                var values: string[] = Array.isArray(properties.value) ? properties.value : [];
+                                var inputValues: string[] = Array.isArray(inputValue) ? inputValue : [];
+                                if (!values.every(elementA => inputValues.includes(elementA))) {
+                                    allConditionsMet = false;
+                                    break;
+                                }
+                                break;
+                            case "contain_at_least_one":
+                                var values: string[] = Array.isArray(properties.value) ? properties.value : [];
+                                var inputValues: string[] = Array.isArray(inputValue) ? inputValue : [];
+                                if (values.some(elementA => inputValues.includes(elementA))) {
+                                    allConditionsMet = false;
+                                    break;
+                                }
+                                break;
+                            case "not_contain_at_least_one":
+                                var values: string[] = Array.isArray(properties.value) ? properties.value : [];
+                                var inputValues: string[] = Array.isArray(inputValue) ? inputValue : [];
+                                if (!values.some(elementA => inputValues.includes(elementA))) {
+                                    allConditionsMet = false;
+                                    break;
+                                }
+                                break;
+                            default:
                                 allConditionsMet = false;
                                 break;
-                            }
-                            break;
-                        case "not_equal":
-                            if (inputValue === properties.value) {
+                    }
+                    } else {
+                        switch (properties.operator) {
+                            case "equal":
+                                if (inputValue !== properties.value) {
+                                    allConditionsMet = false;
+                                    break;
+                                }
+                                break;
+                            case "not_equal":
+                                if (inputValue === properties.value) {
+                                    allConditionsMet = false;
+                                    break;
+                                }
+                                break;
+                            case "contains":
+                                var values: string[] = Array.isArray(properties.value) ? properties.value : [];
+                                var inputValues: string[] = Array.isArray(inputValue) ? inputValue : [];
+                                if (!values.every(elementA => inputValues.includes(elementA))) {
+                                    allConditionsMet = false;
+                                    break;
+                                }
+                                break;
+                            case "not_contains":
+                                var values: string[] = Array.isArray(properties.value) ? properties.value : [];
+                                var inputValues: string[] = Array.isArray(inputValue) ? inputValue : [];
+                                if (values.every(elementA => inputValues.includes(elementA))) {
+                                    allConditionsMet = false;
+                                    break;
+                                }
+                                break;
+                            case "contain_at_least_one":
+                                var values: string[] = Array.isArray(properties.value) ? properties.value : [];
+                                var inputValues: string[] = Array.isArray(inputValue) ? inputValue : [];
+                                if (!values.some(elementA => inputValues.includes(elementA))) {
+                                    allConditionsMet = false;
+                                    break;
+                                }
+                                break;
+                            case "not_contain_at_least_one":
+                                var values: string[] = Array.isArray(properties.value) ? properties.value : [];
+                                var inputValues: string[] = Array.isArray(inputValue) ? inputValue : [];
+                                if (values.some(elementA => inputValues.includes(elementA))) {
+                                    allConditionsMet = false;
+                                    break;
+                                }
+                                break;
+                            default:
                                 allConditionsMet = false;
                                 break;
-                            }
-                            break;
-                        case "contains":
-                            var values: string[] = Array.isArray(properties.value) ? properties.value : [];
-                            var inputValues: string[] = Array.isArray(inputValue) ? inputValue : [];
-                            if (!values.every(elementA => inputValues.includes(elementA))) {
-                                allConditionsMet = false;
-                                break;
-                            }
-                            break;
-                        case "not_contains":
-                            var values: string[] = Array.isArray(properties.value) ? properties.value : [];
-                            var inputValues: string[] = Array.isArray(inputValue) ? inputValue : [];
-                            if (values.every(elementA => inputValues.includes(elementA))) {
-                                allConditionsMet = false;
-                                break;
-                            }
-                            break;
-                        case "contain_at_least_one":
-                            var values: string[] = Array.isArray(properties.value) ? properties.value : [];
-                            var inputValues: string[] = Array.isArray(inputValue) ? inputValue : [];
-                            if (!values.some(elementA => inputValues.includes(elementA))) {
-                                allConditionsMet = false;
-                                break;
-                            }
-                            break;
-                        case "not_contain_at_least_one":
-                            var values: string[] = Array.isArray(properties.value) ? properties.value : [];
-                            var inputValues: string[] = Array.isArray(inputValue) ? inputValue : [];
-                            if (values.some(elementA => inputValues.includes(elementA))) {
-                                allConditionsMet = false;
-                                break;
-                            }
-                            break;
-                        default:
-                            allConditionsMet = false;
-                            break;
+                    }
                     }
                 }
             }
@@ -96,4 +150,12 @@ function getValueFromInput(input: any, attributeName: string): string | string[]
         console.error("Error parsing JSON:", error);
         return undefined;
     }
+}
+
+function isNegativeCondition(node: PathNode): boolean {
+    
+    if (node.type == BlockType.ELSE) {
+        return true
+    }
+    return false
 }
